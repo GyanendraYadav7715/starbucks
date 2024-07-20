9517d6b246d6a26c62cf62d1f824a31194b3156e
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-07-20 18:44:52 +0530

    fourth

---
+++ motion.js
@@ -0,0 +1,11 @@
+import './slider.css'
+
+
+document.querySelectorAll('.slider-item').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        gsap.to(item.querySelector('i'), { scale: 1.2, duration: 0.3 });
+    });
+    item.addEventListener('mouseleave', () => {
+        gsap.to(item.querySelector('i'), { scale: 1, duration: 0.3 });
+    });
+});
