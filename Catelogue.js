94b8020820a22f96efbfa2bb95a9d58c1408e408
DESKTOP-VFB5TE0\Gyanendra Yadav <gyanendrayadav12345789@gmail.com>
2024-07-20 07:32:35 +0530

    boon

--- Catelogue.js
+++ Catelogue.js
@@ -1,27 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuContainer = document.querySelector('.menu-container');
-    const menuItem = document.querySelector('.menu-item');
-    const innerMenu = document.querySelector('.inner-menu');
+    const menuContainers = document.querySelectorAll('.menu-container');
 
-    function handleMouseEnter() {
-        menuContainer.classList.add('hover');
-    }
+    menuContainers.forEach(menuContainer => {
+        const menuItem = menuContainer.querySelector('.menu-item');
+        const innerMenu = menuContainer.querySelector('.inner-menu');
 
-    function handleMouseLeave() {
-        menuContainer.classList.remove('hover');
-    }
-
-    menuItem.addEventListener('mouseenter', handleMouseEnter);
-    menuItem.addEventListener('mouseleave', function (event) {
-        if (!innerMenu.contains(event.relatedTarget)) {
-            handleMouseLeave();
+        function handleMouseEnter() {
+            menuContainer.classList.add('hover');
         }
-    });
 
-    innerMenu.addEventListener('mouseenter', handleMouseEnter);
-    innerMenu.addEventListener('mouseleave', function (event) {
-        if (!menuItem.contains(event.relatedTarget)) {
-            handleMouseLeave();
+        function handleMouseLeave(event) {
+            if (!menuItem.contains(event.relatedTarget) && !innerMenu.contains(event.relatedTarget)) {
+                menuContainer.classList.remove('hover');
+            }
         }
+
+        menuItem.addEventListener('mouseenter', handleMouseEnter);
+        menuItem.addEventListener('mouseleave', handleMouseLeave);
+
+        innerMenu.addEventListener('mouseenter', handleMouseEnter);
+        innerMenu.addEventListener('mouseleave', handleMouseLeave);
     });
 });
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import "./style.css";
 import "./Normalize.css";
+import "./CatelogueM.css"
 
-import { gsap } from "gsap";
+
+
 gsap.registerPlugin(ScrollTrigger);
-
+let Prelodertime = gsap.timeline();
 function loderanimtion() {
-  let Prelodertime = gsap.timeline();
- 
+
+
   function braketheText() {
     let tata = document.querySelector(".starbuck");
     let tatah1 = tata.textContent;
@@ -31,60 +33,63 @@ function loderanimtion() {
     y: 150,
     duration: 0.5,
     stagger: 0.15,
-
   });
   Prelodertime.from(".starbuck .b", {
     opacity: 0,
     y: 150,
     duration: 0.5,
     stagger: 0.15,
-
   });
 
   gsap.from(".preloder img", {
     opacity: 0,
-    duration: .8,
-  })
+    duration: 0.8,
+  });
   Prelodertime.to(".starbuck .a", {
     opacity: 0,
     y: -150,
     duration: 0.4,
-    stagger: 0.10,
-
+    stagger: 0.1,
   });
   Prelodertime.to(".starbuck .b", {
     opacity: 0,
     y: -150,
     duration: 0.4,
-    stagger: 0.10,
-
+    stagger: 0.1,
   });
-  Prelodertime.to(".preloder .goingtoright", {
-    opacity: 0,
-    duration: .7,
-    x: 1000
-  }, 'go')
-  Prelodertime.to(".preloder .goingtoleft", {
-    opacity: 0,
-    duration: .7,
-    x: -1000
-  }, 'go')
+  Prelodertime.to(
+    ".preloder .goingtoright",
+    {
+      opacity: 0,
+      duration: 0.7,
+      x: 1000,
+    },
+    "go"
+  );
+  Prelodertime.to(
+    ".preloder .goingtoleft",
+    {
+      opacity: 0,
+      duration: 0.7,
+      x: -1000,
+    },
+    "go"
+  );
   Prelodertime.to(".preloder", {
-    duration: .2,
+    duration: 0.2,
     opacity: 0,
-    display: "none"
-  })
-
+    display: "none",
+  });
 }
 loderanimtion();
 
 function locomotive() {
-
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector(".app"),
     smooth: true,
   });
   locoScroll.on("scroll", ScrollTrigger.update);
+
   ScrollTrigger.scrollerProxy(".app", {
     scrollTop(value) {
       return arguments.length
@@ -107,88 +112,215 @@ function locomotive() {
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
   ScrollTrigger.refresh();
-
-
 }
 locomotive();
 
-
-
 function landingpage() {
-  let next = document.getElementById('next');
-  let prev = document.getElementById('prev');
-  let carousel = document.querySelector('.carousel');
-  let items = document.querySelectorAll('.carousel .item');
+  let next = document.getElementById("next");
+  let prev = document.getElementById("prev");
+  let carousel = document.querySelector(".carousel");
+  let items = document.querySelectorAll(".carousel .item");
   let countItem = items.length;
   let active = 1;
   let other_1 = null;
   let other_2 = null;
   next.onclick = () => {
-    carousel.classList.remove('prev');
-    carousel.classList.add('next');
+    carousel.classList.remove("prev");
+    carousel.classList.add("next");
     active = active + 1 >= countItem ? 0 : active + 1;
     other_1 = active - 1 < 0 ? countItem - 1 : active - 1;
     other_2 = active + 1 >= countItem ? 0 : active + 1;
     changeSlider();
-  }
+  };
   prev.onclick = () => {
-    carousel.classList.remove('next');
-    carousel.classList.add('prev');
+    carousel.classList.remove("next");
+    carousel.classList.add("prev");
     active = active - 1 < 0 ? countItem - 1 : active - 1;
     other_1 = active + 1 >= countItem ? 0 : active + 1;
     other_2 = other_1 + 1 >= countItem ? 0 : other_1 + 1;
     changeSlider();
-  }
+  };
   const changeSlider = () => {
-    let itemOldActive = document.querySelector('.carousel .item.active');
-    if (itemOldActive) itemOldActive.classList.remove('active');
+    let itemOldActive = document.querySelector(".carousel .item.active");
+    if (itemOldActive) itemOldActive.classList.remove("active");
 
-    let itemOldOther_1 = document.querySelector('.carousel .item.other_1');
-    if (itemOldOther_1) itemOldOther_1.classList.remove('other_1');
+    let itemOldOther_1 = document.querySelector(".carousel .item.other_1");
+    if (itemOldOther_1) itemOldOther_1.classList.remove("other_1");
 
-    let itemOldOther_2 = document.querySelector('.carousel .item.other_2');
-    if (itemOldOther_2) itemOldOther_2.classList.remove('other_2');
+    let itemOldOther_2 = document.querySelector(".carousel .item.other_2");
+    if (itemOldOther_2) itemOldOther_2.classList.remove("other_2");
 
-    items.forEach(e => {
-      e.querySelector('.image img').style.animation = 'none';
-      e.querySelector('.image figcaption').style.animation = 'none';
+    items.forEach((e) => {
+      e.querySelector(".image img").style.animation = "none";
+      e.querySelector(".image figcaption").style.animation = "none";
       void e.offsetWidth;
-      e.querySelector('.image img').style.animation = '';
-      e.querySelector('.image figcaption').style.animation = '';
-    })
+      e.querySelector(".image img").style.animation = "";
+      e.querySelector(".image figcaption").style.animation = "";
+    });
 
-    items[active].classList.add('active');
-    items[other_1].classList.add('other_1');
-    items[other_2].classList.add('other_2');
+    items[active].classList.add("active");
+    items[other_1].classList.add("other_1");
+    items[other_2].classList.add("other_2");
 
     clearInterval(autoPlay);
     autoPlay = setInterval(() => {
       next.click();
     }, 5000);
-  }
+  };
   let autoPlay = setInterval(() => {
     next.click();
   }, 5000);
 
-
-  document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('mouseenter', () => {
+  document.querySelectorAll(".nav-item").forEach((item) => {
+    item.addEventListener("mouseenter", () => {
       gsap.to(item, { scale: 1, duration: 0.3, color: "#40966c" });
     });
 
-    item.addEventListener('mouseleave', () => {
+    item.addEventListener("mouseleave", () => {
       gsap.to(item, { scale: 1, duration: 0.3, color: "black" });
     });
   });
 
+
+  Prelodertime.from(".afterpreloader .logo", {
+    y:30,
+    opacity: 0,
+    duration:.3,
+  })
+  Prelodertime.from(".afterpreloader li", {
+    y: 30,
+    opacity: 0,
+    duration: .7,
+    stagger:0.2
+  })
+  Prelodertime.from(".afterpreloader #nav-part2 h6", {
+    y: 30,
+    opacity: 0,
+    duration: .4,
+    stagger: 0.2
+  })
+  Prelodertime.from(".afterpreloader .content h2", {
+     
+    opacity: 0,
+    duration:2,
+    
+  })
+  Prelodertime.from(".afterpreloader .content p", {
+    x:-50,
+    opacity: 0,
+    duration: 1,
+
+  })
+  Prelodertime.from(".afterpreloader article figure", {
+    
+    opacity: 0,
+    duration:.7,
+
+  })
+  Prelodertime.from(".afterpreloader .content button", {
+    y: -50,
+    opacity: 0,
+    duration: 1,
+
+  })
 }
 landingpage();
 
+function giftanimation() {
+  let gift = gsap.timeline();
+  gift.from(".gift h2", {
+    scale: 0,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".gift",
+      scroller: ".app",
+      // markers: true,
+      start: "bottom 100%",
+      end: "bottom 90%",
+      scrub: true
+    },
+  });
+  gift.from(".gift p", {
+    y: 20,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".gift",
+      scroller: ".app",
+      // markers: true,
+      start: "bottom 100%",
+      end: "bottom 90%",
+      scrub: true
+    },
+  });
+  gift.from(".gift .join", {
+
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".gift",
+      scroller: ".app",
+      // markers: true,
+      start: "bottom 100%",
+      end: "bottom 90%",
+      scrub: true
+    },
+  });
+}
+giftanimation();
+
+function seasonanimatio() {
+
+  gsap.from(".seasonal-heading .zero", {
+    opacity: 0,
+    duration: 1,
+    y: -40,
+    scrollTrigger: {
+      trigger: ".seasonal-heading",
+      scroller: ".app",
+      // markers: true,
+      start: "top 40%",
+      // scrub:3,
+
+    },
+
+  })
+  gsap.from(".seasonal-heading .one", {
+    opacity: 0,
+    duration: 1,
+    delay: 0.2,
+    y: 40,
+    scrollTrigger: {
+      trigger: ".seasonal-heading",
+      scroller: ".app",
+      //markers: true,
+      start: "top 40%",
+      // scrub:3,
+
+    },
+
+  })
+  gsap.from(".Container .box", {
+    opacity: 0,
+    scale: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".Container",
+      scroller: ".app",
+      //markers: true,
+      start: "top 50%",
+      // scrub:3,
+
+    },
+  })
+}
+seasonanimatio();
 
 document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll('footer a');
-  links.forEach(link => {
-    link.classList.add('underline');
+  const links = document.querySelectorAll("footer a");
+  links.forEach((link) => {
+    link.classList.add("underline");
 
     const tl = gsap.timeline({ paused: true });
 
@@ -196,11 +328,10 @@ document.addEventListener("DOMContentLoaded", () => {
       color: "#40966c",
       scale: 1.02,
       duration: 0.3,
-      ease: "power2.inOut"
+      ease: "power2.inOut",
     });
 
-    link.addEventListener('mouseenter', () => tl.play());
-    link.addEventListener('mouseleave', () => tl.reverse());
+    link.addEventListener("mouseenter", () => tl.play());
+    link.addEventListener("mouseleave", () => tl.reverse());
   });
 });
-
