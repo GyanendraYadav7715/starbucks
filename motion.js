a974604120cb838eaef24c9bfaadda21bd16c0c8
Ritik kumar <ritikkumarkashyap9@gmail.com>
2024-07-21 15:13:09 +0530

    sixth commit

--- motion.js
+++ motion.js
@@ -1,11 +1,2 @@
 import './slider.css'
 
-
-document.querySelectorAll('.slider-item').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        gsap.to(item.querySelector('i'), { scale: 1.3, duration: 0.3 });
-    });
-    item.addEventListener('mouseleave', () => {
-        gsap.to(item.querySelector('i'), { scale: 1, duration: 0.3 });
-    });
-});
